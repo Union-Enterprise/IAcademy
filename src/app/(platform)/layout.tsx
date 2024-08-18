cb8800a49e159857c06dce321d5fe84aa8bd7eaf
Caioxz <112172836+Caioxz@users.noreply.github.com>
2024-08-18 17:38:33 -0300

    adicionando o card para ser premium

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
@@ -5,35 +7,48 @@ import Navbar from "../ui/components/Navbar";
 import Sidebar from "../ui/components/Sidebar";
 import { SidebarProvider } from "../context/SidebarContext";
 import { UserProvider } from "../context/UserContext";
+import PremiumCard from "../ui/components/Cardpremium"
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "IAcademy",
-  description: "A sua plataforma de ensino por IA",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [showCard, setShowCard] = useState(false);
+
+  useEffect(() => {
+    const showPremiumCard = () => {
+      setShowCard(true);
+      
+    };
+
+    showPremiumCard();
+
+    const interval = setInterval(showPremiumCard, 180000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <html lang="pt-br">
-      <body
-        className={`${inter.className} h-[100vh] overflow-hidden select-none bg-background-light`}
-      >
-        <UserProvider>
-          <SidebarProvider>
-            <Navbar />
-            <main className="flex w-full h-full">
-              <Sidebar />
-              <section className="gap-10 *:mb-[140px] flex flex-col overflow-auto w-full">
-                {children}
-              </section>
-            </main>
-          </SidebarProvider>
-        </UserProvider>
+      <body className={`${inter.className} h-[100vh] select-none bg-background-light`}>
+        <SidebarProvider>
+          <Navbar />
+          <main className="flex w-full h-full">
+            <Sidebar />
+            <section className="gap-10 *:mb-[140px] flex flex-col overflow-auto w-full">
+              {children}
+              {showCard && (
+                <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+                  <PremiumCard onClose={() => setShowCard(false)} />
+                </div>
+              )}
+            </section>
+          </main>
+        </SidebarProvider>
       </body>
     </html>
   );
