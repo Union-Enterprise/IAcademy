e92fbd87758dc698899b44c9f5ae0d5c1eab88fc
Caioxz <112172836+Caioxz@users.noreply.github.com>
2024-08-20 21:18:31 -0300

    arrumando o erro do card que quando navegava em outras page, ele aparecia, agr n tem mais isso, só quando atualiza a page e dps daqui 30 min

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -1,44 +1,42 @@
-import type { Metadata } from "next";
+"use client"
+
+import { useEffect, useState } from "react";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import Navbar from "../ui/components/Navbar";
 import Sidebar from "../ui/components/Sidebar";
 import { SidebarProvider } from "../context/SidebarContext";
 import { UserProvider } from "../context/UserContext";
-// import PremiumCard from "../ui/components/Cardpremium";
-// import { useEffect, useState } from "react";
+import PremiumCard from "../ui/components/Cardpremium";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "IAcademy",
-  description: "A sua plataforma de ensino por IA",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const [showCard, setShowCard] = useState(false);
+  const [showCard, setShowCard] = useState(false);
 
-  // useEffect(() => {
-  //   const showPremiumCard = () => {
-  //     setShowCard(true);
-  //   };
+  useEffect(() => {
+    const lastShown = localStorage.getItem("lastShownTime");
 
-  //   showPremiumCard();
+    if (!lastShown || Date.now() - parseInt(lastShown) > 1800000) { 
+      setShowCard(true);
+      localStorage.setItem("lastShownTime", Date.now().toString());
+    }
 
-  //   const interval = setInterval(showPremiumCard, 180000);
+    const interval = setInterval(() => {
+      setShowCard(true);
+      localStorage.setItem("lastShownTime", Date.now().toString());
+    }, 1800000);
 
-  //   return () => clearInterval(interval);
-  // }, []);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <html lang="pt-br">
-      <body
-        className={`${inter.className} h-[100vh] select-none bg-background-light overflow-hidden`}
-      >
+      <body className={`${inter.className} h-[100vh] select-none bg-background-light overflow-hidden`}>
         <UserProvider>
           <SidebarProvider>
             <Navbar />
@@ -46,11 +44,11 @@ export default function RootLayout({
               <Sidebar />
               <section className="*:mb-[140px] flex flex-col overflow-auto w-full">
                 {children}
-                {/* {showCard && (
-                <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-                  <PremiumCard onClose={() => setShowCard(false)} />
-                </div>
-              )} */}
+                {showCard && (
+                  <div className="fixed inset-0 flex items-center justify-center z-50">
+                    <PremiumCard onClose={() => setShowCard(false)} />
+                  </div>
+                )}
               </section>
             </main>
           </SidebarProvider>
--- src/app/ui/components/Cardpremium.tsx
+++ src/app/ui/components/Cardpremium.tsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { X } from 'lucide-react';
 
-function PremiumCard({ onClose }) {
+interface PremiumCardProps {
+  onClose: () => void;
+}
+
+function PremiumCard({ onClose }: PremiumCardProps) {
     const [showCloseIcon, setShowCloseIcon] = useState(false);
 
     useEffect(() => {
-       
         const timer = setTimeout(() => {
             setShowCloseIcon(true);
         }, 2000);
@@ -15,75 +18,74 @@ function PremiumCard({ onClose }) {
     }, []);
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-10 h-screen">
-            <div className="relative bg-white rounded-lg w-screen max-w-3xl overflow-hidden">
-              
-                {showCloseIcon && (
-                    <button 
-                        onClick={onClose} 
-                        className="absolute top-4 right-4 z-20 p-2 rounded-full bg-white  focus:outline-none"
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 z-20">
+        <div className="relative bg-white rounded-lg w-screen max-w-3xl overflow-hidden">
+            {showCloseIcon && (
+                <button 
+                    onClick={onClose} 
+                    className="absolute top-4 right-4 z-20 p-2 rounded-full bg-white focus:outline-none"
+                >
+                    <X className="w-6 h-6 text-gray-600 hover:text-red-600" />
+                </button>
+            )}
+
+            <div className="absolute -top-28 -right-10">
+                <svg
+                    width="610"
+                    height="538"
+                    viewBox="0 0 610 538"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-full h-auto"
+                >
+                    <path
+                        d="M684.56 800.598L0.105616 643.602C0.105616 643.602 66.7846 442.279 377.588 321.801C688.391 201.323 755.07 -3.33241e-05 755.07 -3.33241e-05L684.56 800.598Z"
+                        fill="#1865F2" />
+                </svg>
+            </div>
+
+            <div className="absolute -top-20 -right-32">
+                <svg
+                    width="637"
+                    height="538"
+                    viewBox="0 0 637 538"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-full h-auto"
+                >
+                    <path
+                        d="M684.56 764.598L0.105616 607.602C0.105616 607.602 66.7846 406.279 377.588 285.801C688.391 165.323 755.07 -36 755.07 -36L684.56 764.598Z"
+                        fill="#304FFE" />
+                </svg>
+            </div>
+
+            <div className="relative rounded-t-lg p-6 z-10">
+                <h2 className="text-3xl font-bold text-mainBlue">
+                    TRANSFORME SEUS ESTUDOS COM O PLANO PREMIUM
+                </h2>
+            </div>
+            <div className="relative p-6 z-10">
+                <div className="text-gray-700">
+                    <p>Como aluno premium, você não apenas recebe nosso melhor serviço,</p>
+                    <p>mas também acesso exclusivo a benefícios que fazem toda a diferença</p>
+                    <p>na sua experiência conosco.</p>
+                </div>
+                <ul className="text-gray-700 mt-4 space-y-2">
+                    <li>&gt; Ganham módulos diários</li>
+                    <li>&gt; Podem refazer quizzes sem limites</li>
+                    <li>&gt; Mais perguntas à IA</li>
+                </ul>
+                <Link href="/premium">
+                    <button
+                        onClick={onClose}
+                        className="mt-6 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
                     >
-                        <X className="w-6 h-6 text-gray-600 hover:text-red-600" />
+                        ASSINE O PLANO PREMIUM
                     </button>
-                )}
-
-                <div className="absolute -top-28 -right-10">
-                    <svg
-                        width="610"
-                        height="538"
-                        viewBox="0 0 610 538"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-full h-auto"
-                    >
-                        <path
-                            d="M684.56 800.598L0.105616 643.602C0.105616 643.602 66.7846 442.279 377.588 321.801C688.391 201.323 755.07 -3.33241e-05 755.07 -3.33241e-05L684.56 800.598Z"
-                            fill="#1865F2" />
-                    </svg>
-                </div>
-
-                <div className="absolute -top-20 -right-32">
-                    <svg
-                        width="637"
-                        height="538"
-                        viewBox="0 0 637 538"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-full h-auto"
-                    >
-                        <path
-                            d="M684.56 764.598L0.105616 607.602C0.105616 607.602 66.7846 406.279 377.588 285.801C688.391 165.323 755.07 -36 755.07 -36L684.56 764.598Z"
-                            fill="#304FFE" />
-                    </svg>
-                </div>
-
-                <div className="relative rounded-t-lg p-6 z-10">
-                    <h2 className="text-3xl font-bold text-mainBlue">
-                        TRANSFORME SEUS ESTUDOS COM O PLANO PREMIUM
-                    </h2>
-                </div>
-                <div className="relative p-6 z-10">
-                    <div className="text-gray-700">
-                        <p>Como aluno premium, você não apenas recebe nosso melhor serviço,</p>
-                        <p>mas também acesso exclusivo a benefícios que fazem toda a diferença</p>
-                        <p>na sua experiência conosco.</p>
-                    </div>
-                    <ul className="text-gray-700 mt-4 space-y-2">
-                        <li>&gt; Ganham módulos diários</li>
-                        <li>&gt; Podem refazer quizzes sem limites</li>
-                        <li>&gt; Mais perguntas à IA</li>
-                    </ul>
-                    <Link href="/premium">
-                        <button
-                            onClick={onClose}
-                            className="mt-6 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
-                        >
-                            ASSINE O PLANO PREMIUM
-                        </button>
-                    </Link>
-                </div>
+                </Link>
             </div>
         </div>
+    </div>
     );
 }
 
