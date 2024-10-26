808fe4ce017377766bb405095353d0e652631488
Kauã Souza <kaua.souza.santosc@gmail.com>
2024-10-26 01:00:28 -0300

    falta só os conteudos dinamicos agr

--- src/app/(platform)/student/page.tsx
+++ src/app/(platform)/student/page.tsx
@@ -131,7 +131,7 @@ const Student = () => {
         </div>
       </div>
 
-      {isModalOpen && ( // O modal aparece apenas se isModalOpen for true
+      {isModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg shadow-lg p-6 w-1/2">
             {currentQuestion < questions.length ? (
